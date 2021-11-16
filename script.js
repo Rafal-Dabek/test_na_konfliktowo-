d4b0fbe9dc3e286cf45482b16b26cd0c05bec12d
Rafal-Dabek <62549937+Rafal-Dabek@users.noreply.github.com>
2021-11-16 13:30:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -50,13 +50,7 @@ function show_table()
 
 var is_pressed=false;
 function done(){
-  var mentor=0;
-  var A=0;
-  var B=0;
-  var C=0;
-  var D=0;
-  var E=0;
-  var F=0;
+  var suma=0;
   
   var ele = document.getElementsByTagName('input');
     
@@ -87,51 +81,20 @@ function done(){
 
   var p=Array.from(d);
  
-for(i=0;i<d.length;i++){
 
-if(d[i]=="A")A++; 
-if(d[i]=="B")B++; 
-if(d[i]=="C")C++; 
-if(d[i]=="D")D++;
-if(d[i]=="E")E++; 
-if(d[i]=="F")F++;
-}
 
-if((A+B+C+D+E+F)!=21){
+if((d.length)!=11){
   alert("Nie wszytkie odpowiedzi są zaznaczone")
 }
 
-if(A+B+C+D+E+F==21 && (is_pressed==false)){
+if(d.length==11){
+  for(i=0;i<p.length;i++){
+
+    suma+=parseInt(p[i]);
+    }
   is_pressed=true;
-  console.log(is_pressed);
-  if(A>C){
-    document.getElementById("punkty_1").innerHTML
-    +=  "W";
-   
-  }
-  if(A<C){
-    document.getElementById("punkty_1").innerHTML
-    +=  "K";
-    
-  }
-  if(D>F){
-    document.getElementById("punkty_1").innerHTML
-    +=  "S";
-  }
-  if(D<F){
-    document.getElementById("punkty_1").innerHTML
-    +=  "Z";
-  }
-  if(B>E){
-    document.getElementById("punkty_1").innerHTML
-    +=  "W";
-    
-  }
-  if(B<E){
-    document.getElementById("punkty_1").innerHTML
-    +=  "Z";
-   
-  }
+  console.log(suma);
+  
 
 }
 
